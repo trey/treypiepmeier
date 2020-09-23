62f31c7a9233e6ba5a5922fec673b3bffeaaad60
Trey Piepmeier <trey@treypiepmeier.com>
2020-09-23 10:29:37 -0400

    Update stylelint configuration.
    Those files have moved around.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,5 @@ module.exports = {
         "string-quotes": "single",
         "at-rule-no-unknown": null,
     },
-    "ignoreFiles": "src/_includes/scss/vendor/**",
+    "ignoreFiles": "src/scss/vendor/**",
 }
