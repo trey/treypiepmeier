1753624608700009f6f04cf1ec6fee3e78b96a6b
Trey Piepmeier <trey@treypiepmeier.com>
2021-06-28 13:51:46 -0400

    Actually sort stuff from Pinboard.
    Hopefully this’ll work the same locally and in production.
    
    javascript - sort JSON by date - Stack Overflow:
    https://stackoverflow.com/a/3859297

--- src/_data/links.js
+++ src/_data/links.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 module.exports = async () => {
     const { data } = await axios.get(`https://api.pinboard.in/v1/posts/recent?tag=tp&auth_token=${process.env.PINBOARD_AUTH}&format=json`);
 
-    return data.posts;
+    return data.posts.sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime());
 };
