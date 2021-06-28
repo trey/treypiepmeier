4e95591ec7a868af67ef3a92d2b757a35ee072e7
Trey Piepmeier <trey@treypiepmeier.com>
2021-06-28 14:19:38 -0400

    Get more than the most recent 15 bookmarks.

--- src/_data/links.js
+++ src/_data/links.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 module.exports = async () => {
-    const { data } = await axios.get(`https://api.pinboard.in/v1/posts/recent?tag=tp&auth_token=${process.env.PINBOARD_AUTH}&format=json`);
+    const { data } = await axios.get(`https://api.pinboard.in/v1/posts/all?tag=tp&auth_token=${process.env.PINBOARD_AUTH}&format=json`);
 
-    return data.posts.sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime());
+    return data.sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime());
 };
