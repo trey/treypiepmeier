a1461222979b2c35655b4c8bdc8b81b63dd4c5df
Trey Piepmeier <trey@treypiepmeier.com>
2021-06-29 09:38:06 -0400

    Try node fetch instead of Axios.
    No idea if it’ll actually sort correctly on Netlify, but I guess it’s
    worth a shot.
    
    https://www.11ty.dev/docs/quicktips/eliminate-js/
    
    https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await

--- src/_data/links.js
+++ src/_data/links.js
@@ -1,8 +1,8 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
-module.exports = async () => {
-    const { data } = await axios.get(`https://api.pinboard.in/v1/posts/all?tag=tp&auth_token=${process.env.PINBOARD_AUTH}&format=json`);
-
-    return data.sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime());
+module.exports = async function() {
+    return await fetch(`https://api.pinboard.in/v1/posts/all?tag=tp&auth_token=${process.env.PINBOARD_AUTH}&format=json`)
+        .then(res => res.json())
+        .then(json => json.sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime()));
 };
