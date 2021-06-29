9ed0cf167793c2ed77def4c6b69e99ad5d59b7e4
Trey Piepmeier <trey@treypiepmeier.com>
2021-06-29 11:09:32 -0400

    Try Luxon.

--- src/_data/links.js
+++ src/_data/links.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
+const { DateTime } = require('luxon');
 require('dotenv').config();
 
 module.exports = async () => {
     const { data } = await axios.get(`https://api.pinboard.in/v1/posts/all?tag=tp&auth_token=${process.env.PINBOARD_AUTH}&format=json`);
 
-    return data.sort((a, b) => Date.parse(a.time) - Date.parse(b.time));
+    return data.sort(
+        (a, b) => DateTime.fromISO(a.time, { zone: 'utc' }) - DateTime.fromISO(b.time, { zone: 'utc' })
+    );
 };
