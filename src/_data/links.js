9249fb49a6ab829e2ecf4a4a1ca7a9906bb31e9e
Trey Piepmeier <trey@treypiepmeier.com>
2021-06-29 11:01:39 -0400

    Back to Axios, try `Date.parse(…)`…
    …instead of `new Date(…)`

--- src/_data/links.js
+++ src/_data/links.js
@@ -1,8 +1,8 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 require('dotenv').config();
 
-module.exports = async function() {
-    return await fetch(`https://api.pinboard.in/v1/posts/all?tag=tp&auth_token=${process.env.PINBOARD_AUTH}&format=json`)
-        .then(res => res.json())
-        .then(json => json.sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime()));
+module.exports = async () => {
+    const { data } = await axios.get(`https://api.pinboard.in/v1/posts/all?tag=tp&auth_token=${process.env.PINBOARD_AUTH}&format=json`);
+
+    return data.sort((a, b) => Date.parse(a.time) - Date.parse(b.time));
 };
